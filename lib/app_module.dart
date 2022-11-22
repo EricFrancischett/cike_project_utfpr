9bf75f7660eba819065b353af7416a933540c919
EricFrancischett <ericmellof@gmail.com>
2022-11-22 10:55:18 -0300

    error solved

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,6 +1,4 @@
-import 'package:cike_project_utfpr/features/home/controller/home_controller.dart';
 import 'package:cike_project_utfpr/features/home/home_module.dart';
-import 'package:cike_project_utfpr/features/home/view/home_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
